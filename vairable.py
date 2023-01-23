5f70a26b1dc3de2866e550f2002c0d2ad47eafca
Emmanuel <emmanuelseun078@gmail.com>
2023-01-23 11:13:17 +0100

    Day Three : Print And Input Method

--- vairable.py
+++ vairable.py
@@ -1,5 +1,23 @@
-num_one = 30
+# num_one = 30
 
-num_two = 20
+# num_two = 30
 
-sum_num = 50
+# sum_num = num_one+num_two
+
+# # print (sum_num)
+
+# name ="john smith"
+
+# age = 40
+
+# is_new = True
+
+# print ('hello world \ni am seun and i am 16 years old')
+
+# print ("welcome "+ name + ", you are " + str(age) +" years old, and a new user: "+ str(is_new))
+
+# name = input("Enter name: ")
+
+# print("Welcome "+ name) 
+
+print("Twinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.\nTwinkle, twinkle, little star,\nHow I wonder what you")
